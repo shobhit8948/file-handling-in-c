3e3e98e256a2ec3f711ba23d1d7d244b76f5c553
shobhit8948 <113876252+shobhit8948@users.noreply.github.com>
2024-01-25 10:27:00 +0000

    file creation

--- .ccls-cache/@home@runner@file-handling-in-c/main.c
+++ .ccls-cache/@home@runner@file-handling-in-c/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main() {
+    FILE *file = fopen("myfile.txt", "w"); // "w" for write mode
+    if (file != NULL) {
+        // file created successfully
+        fclose(file); // remember to close the file
+    } else {
+        // failed to create the file
+    }
+    return 0;
 }
