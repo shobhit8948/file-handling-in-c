3a6f8b3e2d4ee9df9706f2db9351c2f86d9ff4c2
shobhit8948 <113876252+shobhit8948@users.noreply.github.com>
2024-01-26 03:35:04 +0000

    final

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// how to create a file
 #include <stdio.h>
 
 int main() {
@@ -8,5 +9,101 @@ int main() {
     } else {
         // failed to create the file
     }
+    return 0;
+}
+
+// how to read a character
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr= fopen("myfile.txt","r");
+    char ch= fgetc(fptr);
+    printf("The character I read is %c",ch);
+    ch= fgetc(fptr);
+    printf("The character I read is %c",ch);
+    fclose(fptr);
+    return 0;
+}
+
+// how to read a string
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr= fopen("myfile.txt","r");
+    char str[100];
+    fgets(str,7,fptr);
+    printf("The character I read is %s",str);
+    fclose(fptr);
+    return 0;
+}
+
+// how to write a character
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr= fopen("myfile.txt","w");
+    char ch= fputc('A',fptr);
+    printf("The character I read is %c",ch);
+    fclose(fptr);
+    return 0;
+}
+
+// how to write a string
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr= fopen("myfile.txt","w");
+    char str[100]= "Hello World";
+    fputs(str,fptr);
+    printf("The character I read is %s",str);
+    fclose(fptr);
+    return 0;
+}
+
+//how to append in file
+// #include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr= fopen("myfile.txt","a");
+    char str[100]= " Hello World";
+    fputs(str,fptr);
+    printf("The character I read is %s",str);
+    fclose(fptr);
+    return 0;
+}
+
+// how to use r+ in a file
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr= fopen("myfile.txt","r+");
+    char str[100]= " Hello World";
+    fputs(str,fptr);
+    printf("The character I read is %s",str);
+    fclose(fptr);
+    return 0;
+}
+
+//how to use w+ in a file
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr= fopen("myfile.txt","w+");
+    char str[100]= " Hello aviral";
+    fputs(str,fptr);
+    printf("The character I read is %s",str);
+    fclose(fptr);
+    return 0;
+}
+
+//how to use a+ in a file
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr= fopen("myfile.txt","a+");
+    char str[100]= " welcome aviral";
+    fputs(str,fptr);
+    printf("The character I read is %s",str);
+    fclose(fptr);
     return 0;
 }
